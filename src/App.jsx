e38daed248e47258e8490752ceffe9ffac295229
vachito <vacho19@gmail.com>
2026-07-04 10:35:15 -0600

    creando un state e inicializndolo con un archivo json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import {useState} from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import {db} from "./data/db";
 
 function App() {
+  const [data, setData] = useState(db);
+
   return (
     <>
       <Header/> 
