8c8b712cfb77c79ec095fc7c1c7555c78579890f
vachito <vacho19@gmail.com>
2026-07-04 12:53:14 -0600

    mostrando las guitarras en el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ function App() {
 
   return (
     <>
-      <Header/> 
+      <Header
+        cart={cart}
+      /> 
     
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
